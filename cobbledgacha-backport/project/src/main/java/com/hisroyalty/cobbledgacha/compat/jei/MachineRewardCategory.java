7adfc5d17c658cd434e1eb5fb99f869ac846fcf2
acuario22-cpu <acuario04.xdn@gmail.com>
2026-09-25 13:38:30 -0500

    Add JEI reward browser for CobbledGacha

---
+++ cobbledgacha-backport/project/src/main/java/com/hisroyalty/cobbledgacha/compat/jei/MachineRewardCategory.java
@@ -0,0 +1,45 @@
+package com.hisroyalty.cobbledgacha.compat.jei;
+
+import com.hisroyalty.cobbledgacha.CobbledGacha;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+
+public class MachineRewardCategory implements IRecipeCategory<MachineRewardRecipe> {
+    public static final RecipeType<MachineRewardRecipe> TYPE = RecipeType.create(CobbledGacha.MOD_ID, "machine_rewards", MachineRewardRecipe.class);
+    private final IDrawable background, icon;
+
+    public MachineRewardCategory(IGuiHelper gui) {
+        background = gui.createBlankDrawable(170, 52);
+        icon = gui.createDrawableIngredient(mezz.jei.api.constants.VanillaTypes.ITEM_STACK,
+            new ItemStack(CobbledGacha.MACHINES.get("gacha_machine").get()));
+    }
+
+    @Override public RecipeType<MachineRewardRecipe> getRecipeType() { return TYPE; }
+    @Override public Component getTitle() { return Component.translatable("jei.cobbledgacha.machine_rewards"); }
+    @Override public IDrawable getBackground() { return background; }
+    @Override public IDrawable getIcon() { return icon; }
+
+    @Override
+    public void setRecipe(IRecipeLayoutBuilder builder, MachineRewardRecipe recipe, IFocusGroup focuses) {
+        builder.addSlot(RecipeIngredientRole.CATALYST, 7, 20).addItemStack(recipe.machine());
+        builder.addSlot(RecipeIngredientRole.INPUT, 48, 20).addItemStacks(recipe.currencies());
+        builder.addSlot(RecipeIngredientRole.OUTPUT, 140, 20).addItemStack(recipe.reward());
+    }
+
+    @Override
+    public void draw(MachineRewardRecipe recipe, IRecipeSlotsView slots, GuiGraphics graphics, double mouseX, double mouseY) {
+        var font = Minecraft.getInstance().font;
+        graphics.drawString(font, Component.literal(recipe.cost() + "x"), 70, 24, 0x404040, false);
+        graphics.drawString(font, Component.literal(String.format(java.util.Locale.ROOT, "%.2f%%", recipe.chance())), 94, 24, 0x404040, false);
+    }
+}
