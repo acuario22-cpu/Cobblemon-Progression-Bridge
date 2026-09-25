50dc87bdc71fff014521ca9d6b1994f0bbc6b3de
acuario22-cpu <acuario04.xdn@gmail.com>
2026-09-25 13:38:13 -0500

    Add JEI reward browser for CobbledGacha

---
+++ cobbledgacha-backport/project/src/main/java/com/hisroyalty/cobbledgacha/compat/jei/CapsuleRewardRecipe.java
@@ -0,0 +1,5 @@
+package com.hisroyalty.cobbledgacha.compat.jei;
+
+import net.minecraft.world.item.ItemStack;
+
+public record CapsuleRewardRecipe(ItemStack capsule, ItemStack reward, float chance) {}
