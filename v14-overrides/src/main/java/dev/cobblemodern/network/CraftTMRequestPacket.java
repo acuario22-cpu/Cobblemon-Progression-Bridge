9e07237c9bc80fa94db9298298d5a6a6486930a0
acuario22-cpu <acuario04.xdn@gmail.com>
2026-09-25 13:00:22 -0500

    Add v1.4 blank TM and material state network

---
+++ v14-overrides/src/main/java/dev/cobblemodern/network/CraftTMRequestPacket.java
@@ -0,0 +1,30 @@
+package dev.cobblemodern.network;
+
+import dev.cobblemodern.tm.TMSystem;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.network.NetworkEvent;
+
+import java.util.function.Supplier;
+
+public record CraftTMRequestPacket(String moveId) {
+    public static void encode(CraftTMRequestPacket packet, FriendlyByteBuf buf) {
+        buf.writeUtf(packet.moveId, 128);
+    }
+
+    public static CraftTMRequestPacket decode(FriendlyByteBuf buf) {
+        return new CraftTMRequestPacket(buf.readUtf(128));
+    }
+
+    public static void handle(CraftTMRequestPacket packet, Supplier<NetworkEvent.Context> contextSupplier) {
+        NetworkEvent.Context context = contextSupplier.get();
+        ServerPlayer sender = context.getSender();
+        if (sender != null) {
+            context.enqueueWork(() -> {
+                TMSystem.craft(sender, packet.moveId);
+                TMSystem.openMachine(sender);
+            });
+        }
+        context.setPacketHandled(true);
+    }
+}
