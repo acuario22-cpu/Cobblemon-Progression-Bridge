7716b0eec6ffa05e45a5de9b09024cd32d63c4a7
acuario22-cpu <acuario04.xdn@gmail.com>
2026-09-25 15:35:00 -0500

    Batch Pokémon rewards for JEI

--- cobbledgacha-backport/project/src/main/java/com/hisroyalty/cobbledgacha/compat/jei/PokemonGachaRecipe.java
+++ cobbledgacha-backport/project/src/main/java/com/hisroyalty/cobbledgacha/compat/jei/PokemonGachaRecipe.java
@@ -3,5 +3,4 @@ package com.hisroyalty.cobbledgacha.compat.jei;
 import net.minecraft.world.item.ItemStack;
 import java.util.List;
 
-public record PokemonGachaRecipe(ItemStack machine, List<ItemStack> currencies, String species, String bucket,
-                                 int minLevel, int maxLevel, float chance, int cost) {}
+public record PokemonGachaRecipe(ItemStack machine, List<ItemStack> currencies, List<PokemonRewardLine> entries, int cost) {}
